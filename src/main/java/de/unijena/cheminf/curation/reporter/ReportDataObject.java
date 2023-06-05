8022d401e610b7818627b4aca958aeebb3473880
SamuelBehr <samuel.behr@gmx.de>
2023-06-05 23:01:17 +0200

    wip; fails multiple tests; some progress regarding the reporting basis, specifically the ReportDataObject class; implemented an ErrorCodes enum;

--- src/main/java/de/unijena/cheminf/curation/reporter/ReportDataObject.java
+++ src/main/java/de/unijena/cheminf/curation/reporter/ReportDataObject.java
@@ -25,7 +25,11 @@
 
 package de.unijena.cheminf.curation.reporter;
 
+import de.unijena.cheminf.curation.ErrorCodes;
+import de.unijena.cheminf.curation.IProcessingStep;
 import org.openscience.cdk.interfaces.IAtomContainer;
+
+import java.util.Objects;
 
 /**
  * Object that stores all data necessary for an entry in a report file.
@@ -33,10 +37,9 @@ import org.openscience.cdk.interfaces.IAtomContainer;
 public class ReportDataObject {
 
     /*
-    TODO: choose a more specific class name?
-    TODO: implement an IReport interface?
-    //
     TODO: parameter checks?
+    TODO: how to access the parent structure(s)?
+    TODO: explicitly store the identifiers?
      */
 
     /**
@@ -45,41 +48,63 @@ public class ReportDataObject {
     private final IAtomContainer atomContainer;
 
     /**
-     * Integer identifier of the structure the report refers to.
+     * Identifier string of the structure the report refers to.
      */
-    private final int identifier;
+    private final String identifier;
 
     /**
-     * Optional second integer identifier of the structure the report refers to or null if there is none.
+     * Optional second identifier string of the structure the report refers to or null if there is none.
      */
-    private final Integer optionalIdentifier;
+    private final String optionalIdentifier;
 
     /**
-     * String description of the processing step the reported problem occurred in.
+     * String with the index of the processing step instance in which the reported problem occurred in the pipeline, or
+     * null if the processing step is not part of a pipeline.
      */
-    private final String processingStepDescription;
+    private final String indexOfProcessingStepInPipeline;
 
     /**
-     * The reported exception.
+     * Runtime class of the IProcessingStep instance the reported problem occurred in.
      */
-    private final Exception reportedException;
+    private final Class<? extends IProcessingStep> classOfProcessingStep;
 
     /**
+     * Error code of the reported problem.
+     */
+    private final ErrorCodes errorCode;
+
+    /** TODO
      * Constructor. Initializes all class variables.
      *
      * @param anAtomContainer atom container of the structure
-     * @param anIdentifier integer identifier of the structure
-     * @param anOptionalIdentifier optional second integer identifier of the structure or null if there is none
-     * @param aProcessingStepDescription string describing the processing step the reported problem occurred in
-     * @param anException exception to be reported
+     * @param anIdentifier identifier string of the structure
+     * @param anOptionalIdentifier optional second identifier string of the structure or null if there is none
+     * @param anIndexOfProcessingStepInPipeline string of the index of the processing step the in the pipeline or null
+     *                                          if the is not part of a pipeline
+     * @param classOfProcessingStep runtime class of the IProcessingStep instance reporting the problem
+     * @param anErrorCode error code of the reported problem
+     * @throws NullPointerException if the given anAtomContainer, anIdentifier, classOfProcessingStep or anErrorCode
+     *                              is null
      */
-    public ReportDataObject(IAtomContainer anAtomContainer, int anIdentifier, Integer anOptionalIdentifier,
-                            String aProcessingStepDescription, Exception anException) {
+    public ReportDataObject(
+            IAtomContainer anAtomContainer,
+            String anIdentifier,
+            String anOptionalIdentifier,
+            String anIndexOfProcessingStepInPipeline,
+            Class<? extends IProcessingStep> classOfProcessingStep,
+            ErrorCodes anErrorCode
+    ) throws NullPointerException {
+        Objects.requireNonNull(anAtomContainer, "anAtomContainer (instance of IAtomContainer) is null.");
+        Objects.requireNonNull(anIdentifier, "anIdentifier (instance of String) is null.");
+        Objects.requireNonNull(classOfProcessingStep, "classOfProcessingStep (instance of Class<? extends" +
+                " IProcessingStep>) is null.");
+        Objects.requireNonNull(anErrorCode, "anErrorCode (instance of ErrorCodes) is null.");
         this.atomContainer = anAtomContainer;
         this.identifier = anIdentifier;
         this.optionalIdentifier = anOptionalIdentifier;
-        this.processingStepDescription = aProcessingStepDescription;
-        this.reportedException = anException;
+        this.indexOfProcessingStepInPipeline = anIndexOfProcessingStepInPipeline;
+        this.classOfProcessingStep = classOfProcessingStep;
+        this.errorCode = anErrorCode;
     }
 
     /**
@@ -92,39 +117,49 @@ public class ReportDataObject {
     }
 
     /**
-     * Returns the integer identifier of the structure the report refers to.
+     * Returns the identifier string of the structure the report refers to.
      *
-     * @return integer value
+     * @return String
      */
-    public int getIdentifier() {
+    public String getIdentifier() {
         return identifier;
     }
 
     /**
-     * Returns the optional second integer identifier of the structure the report refers to or null if none was given.
+     * Returns the optional second identifier string of the structure the report refers to or null if none was given.
      *
      * @return integer value or null
      */
-    public Integer getOptionalIdentifier() {
+    public String getOptionalIdentifier() {
         return optionalIdentifier;
     }
 
     /**
-     * Returns a string description of the processing step the reported problem occurred in.
+     * Returns a string with the index of the processing step instance the problem was reported by in the pipeline, or
+     * null if the processing step is not part of a pipeline.
      *
      * @return instance of String
      */
-    public String getProcessingStepDescription() {
-        return processingStepDescription;
+    public String getIndexOfProcessingStepInPipeline() {
+        return indexOfProcessingStepInPipeline;
     }
 
     /**
-     * Returns the exception to be reported.
+     * Returns the runtime class of the IProcessingStep instance the reported problem occurred in.
      *
-     * @return instance of String
+     * @return instance of Class<? extends IProcessingStep>
      */
-    public Exception getReportedException() {
-        return reportedException;
+    public Class<? extends IProcessingStep> getClassOfProcessingStep() {
+        return classOfProcessingStep;
+    }
+
+    /**
+     * Returns the error code of the reported problem.
+     *
+     * @return instance of ErrorCodes
+     */
+    public ErrorCodes getErrorCode() {
+        return errorCode;
     }
 
 }
